5bab6488b1f3a7bd1619c86c031253d857f25c9b
TomHemelsoen <61147865+TomHemelsoen@users.noreply.github.com>
2020-05-18 18:22:33 +0200

    website

---
+++ assignments/assignment3/website/firstpage/js/script.js
@@ -0,0 +1,6 @@
+const toggleNavigationIcon = (navToggler) => {
+    const navigation= document.querySelector('.border')
+navigation.classList.toggle('toggled')
+    navToggler.classList.toggle('toggled')
+}
+
